8a34381d9991d280bfc3356f2fd1ad73fb81d30d
Patrick Hein <bagbag98@googlemail.com>
2019-03-27 10:21:24 +0100

    implement async-writable-stream

---
+++ server/src/utils/async-writable-stream.ts
@@ -0,0 +1,61 @@
+import { DeferredPromise } from '@common-ts/base/promise';
+import { Writable } from 'stream';
+
+type NonObjectMode = {
+  readonly _nonObjectMode: unique symbol
+};
+
+export type Data<T> = T extends NonObjectMode ? (string | Buffer | Uint8Array) : (T extends null ? never : T);
+
+export class AsyncWritableStream<T = NonObjectMode> {
+  private readonly writable: Writable;
+  private readonly drain: DeferredPromise;
+
+  private hasError: boolean;
+  private error: Error | undefined;
+
+  constructor(writable: Writable) {
+    this.writable = writable;
+
+    this.drain = new DeferredPromise();
+    this.hasError = false;
+    this.error = undefined;
+
+    this.writable.on('error', (error) => this.handleError(error));
+    this.writable.on('drain', () => this.handleDrain());
+  }
+
+  async write(data: Data<T>, encoding?: BufferEncoding): Promise<void> {
+    this.checkError();
+
+    const canContinue = this.writable.write(data, encoding);
+
+    if (!canContinue) {
+      await this.drain;
+    }
+
+    this.checkError();
+  }
+
+  async end(): Promise<void> {
+    this.checkError();
+
+    return new Promise<void>((resolve) => this.writable.end(resolve));
+  }
+
+  private checkError(): void {
+    if (this.hasError) {
+      throw this.error;
+    }
+  }
+
+  private handleError(error: Error): void {
+    this.hasError = true;
+    this.error = error;
+  }
+
+  private handleDrain(): void {
+    this.drain.resolve();
+    this.drain.reset();
+  }
+}
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,3 +1,5 @@
+export * from './async-iterable-stream';
 export * from './async-stream-iterable';
+export * from './async-writable-stream';
 export * from './stream-reader';
 export * from './unique-id';
