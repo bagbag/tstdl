ea720be5a5e214a1b54d0d596771327d7ff411d7
Patrick Hein <bagbag98@googlemail.com>
2019-04-10 12:29:11 +0200

    fixes TypedIndexSpecification

--- mongo/src/types.ts
+++ mongo/src/types.ts
@@ -2,5 +2,5 @@ import { IndexSpecification } from 'mongodb';
 import { Omit } from '@common-ts/base/types';
 
 export type TypedIndexSpecification<T> = Omit<IndexSpecification, 'key'> & {
-  key: { [P in keyof T]: 1 | -1 | 'text' | 'hashed' }
+  key: { [P in keyof T]?: 1 | -1 | 'text' | 'hashed' }
 };
