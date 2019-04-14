b47cbadba6aca99fbbf0d036b89520ae4fc1eab2
Patrick Hein <bagbag98@googlemail.com>
2019-04-14 21:45:46 +0200

    uniqueId async version and performance improvements

--- server/src/utils/unique-id.ts
+++ server/src/utils/unique-id.ts
@@ -1,18 +1,58 @@
-import { randomBytes } from 'crypto';
 import { zBase32Encode } from '@common-ts/base/utils';
+import { randomBytes } from 'crypto';
 
-export function uniqueId(): string {
-  const bytes = randomBytes(16);
-  const id = zBase32Encode(bytes);
+let index = 0;
+let buffer = new Uint8Array();
+
+export async function uniqueId(bufferSize: number): Promise<string> {
+  const buffer = await getRandomBytes(bufferSize);
+  const id = zBase32Encode(buffer);
 
   return id;
 }
 
-function numberToBuffer(value: number): Buffer {
-  const bytes = Math.ceil(Math.log2(value + 1) / 8);
+export function uniqueIdSync(bufferSize: number): string {
+  const buffer = getRandomBytesSync(bufferSize);
+  const id = zBase32Encode(buffer);
 
-  const buffer = Buffer.allocUnsafe(bytes);
-  buffer.writeUIntLE(value, 0, bytes);
+  return id;
+}
 
-  return buffer;
+async function getRandomBytes(count: number): Promise<ArrayBuffer> {
+  if (count > (buffer.byteLength - index)) {
+    buffer = await randomBytesAsync(Math.max(count, 1024));
+    index = 0;
+  }
+
+  const end = index + count;
+  const bytes = buffer.slice(index, end);
+  index = end;
+
+  return bytes;
+}
+
+function getRandomBytesSync(count: number): ArrayBuffer {
+  if (count > (buffer.byteLength - index)) {
+    buffer = randomBytes(Math.max(count, 1024));
+    index = 0;
+  }
+
+  const end = index + count;
+  const bytes = buffer.slice(index, end);
+  index = end;
+
+  return bytes;
+}
+
+async function randomBytesAsync(count: number): Promise<Buffer> {
+  return new Promise<Buffer>((resolve, reject) => {
+    randomBytes(count, (error, buffer) => {
+      if (error != undefined) {
+        reject(error);
+        return;
+      }
+
+      resolve(buffer);
+    })
+  });
 }
