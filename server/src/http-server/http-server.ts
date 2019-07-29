fde0bc0b5c678b6d2d3aad600f631e5a81cf9cee
Patrick Hein <bagbag98@googlemail.com>
2019-07-29 11:12:34 +0200

    adds http server abstraction

---
+++ server/src/http-server/http-server.ts
@@ -0,0 +1,91 @@
+import { Logger } from '@tstdl/base/logger';
+import { cancelableTimeout, Timer } from '@tstdl/base/utils';
+import * as Http from 'http';
+import { Socket } from 'net';
+
+export class HttpServer {
+  private readonly logger: Logger;
+  private readonly sockets: Set<Socket>;
+
+  private untrackConnectedSockets: () => void;
+
+  readonly server: Http.Server;
+
+  get connectedSockets(): Iterable<Socket> {
+    return this.sockets.values();
+  }
+
+  constructor(logger: Logger) {
+    this.logger = logger;
+
+    this.server = new Http.Server();
+    this.sockets = new Set();
+  }
+
+  listen(port: number): void {
+    if (this.server.listening) {
+      throw new Error('already listening');
+    }
+
+    this.untrackConnectedSockets = trackConnectedSockets(this.server, this.sockets);
+    this.server.listen(port);
+  }
+
+  async close(timeout: number): Promise<void> {
+    const timer = new Timer(true);
+
+    const closePromise = new Promise<void>((resolve, reject) => this.server.close((error) => error != undefined ? reject(error) : resolve(undefined)));
+
+    while (true) {
+      const connections = await getConnectionsCount(this.server);
+
+      if (connections == 0) {
+        break;
+      }
+
+      if (timer.milliseconds >= timeout) {
+        this.logger.info(`force closing of remaining sockets after waiting for ${timeout} milliseconds`);
+        destroySockets(this.sockets);
+        break;
+      }
+
+      if (connections > 0) {
+        this.logger.info(`waiting for ${connections} to end`);
+        await cancelableTimeout(1000, closePromise);
+      }
+    }
+
+    this.untrackConnectedSockets();
+  }
+
+  registerRequestHandler(handler: (request: Http.IncomingMessage, response: Http.ServerResponse) => void) {
+    this.server.on('request', handler);
+  }
+}
+
+function trackConnectedSockets(server: Http.Server, sockets: Set<Socket>): () => void {
+  const connectionListener = (socket: Socket) => {
+    sockets.add(socket);
+
+    const closeListener = () => {
+      sockets.delete(socket);
+      socket.removeListener('close', closeListener);
+    };
+
+    socket.on('close', closeListener);
+  };
+
+  server.on('connection', connectionListener);
+
+  return () => server.removeListener('connection', connectionListener);
+}
+
+async function getConnectionsCount(server: Http.Server): Promise<number> {
+  return new Promise<number>((resolve, reject) => server.getConnections((error, count) => error != undefined ? reject(error) : resolve(count)));
+}
+
+function destroySockets(sockets: Iterable<Socket>): void {
+  for (const socket of sockets) {
+    socket.destroy();
+  }
+}
