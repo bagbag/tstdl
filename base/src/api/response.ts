03f1b53026cc7e03afb0b4defb3cb28339b4cbf7
Patrick Hein <bagbag98@googlemail.com>
2019-09-17 16:12:18 +0200

    allows undefined for error-data

--- base/src/api/response.ts
+++ base/src/api/response.ts
@@ -2,7 +2,7 @@ import { UndefinableJson } from '../types';
 import { ApiError } from './error';
 
 export type ErrorSerializer<T extends Error, TData extends UndefinableJson> = (error: T) => TData;
-export type ErrorDeserializer<T extends Error, TData extends UndefinableJson> = (data: TData, responseError: ResponseError) => T;
+export type ErrorDeserializer<T extends Error, TData extends undefined | UndefinableJson> = (data: TData, responseError: ResponseError) => T;
 export type ErrorHandler<T extends Error = Error, TData extends UndefinableJson = UndefinableJson> = { statusCode: number, serializer: ErrorSerializer<T, TData>, deserializer: ErrorDeserializer<T, TData> };
 
 export type ResultResponse<T> = {
