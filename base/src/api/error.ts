f92b805da70a46e489e38aa5addd8b3258f10f01
Patrick Hein <bagbag98@googlemail.com>
2019-09-18 12:27:05 +0200

    removes using error name for api-error

--- base/src/api/error.ts
+++ base/src/api/error.ts
@@ -5,7 +5,7 @@ export class ApiError extends CustomError {
   readonly details: any;
 
   constructor(response: ErrorResponse) {
-    super({ message: response.error.message, name: response.error.name });
+    super({ message: `${response.error.name}: ${response.error.message}` });
 
     this.details = response.error.details;
   }
