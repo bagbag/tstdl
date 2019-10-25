e62a2706edfeaf5e7e5594c17d41b91a7cf784be
Patrick Hein <hein@solutec.de>
2019-10-25 10:00:29 +0200

    bumps versions

--- mongo/src/mongo-base-repository.ts
+++ mongo/src/mongo-base-repository.ts
@@ -91,8 +91,8 @@ export class MongoBaseRepository<T extends Entity> {
     return savedEntities;
   }
 
-  async update<U extends T>(filter: FilterQuery<U>, update: MongoDocument<U> | UpdateQuery<U>): Promise<void> {
-    this.collection.updateOne(filter, update as MongoDocument<U> | UpdateQuery<T>);
+  async update<U extends T>(filter: FilterQuery<U>, update: Partial<MongoDocument<U>> | UpdateQuery<U>): Promise<void> {
+    this.collection.updateOne(filter, update);
   }
 
   async load<U extends T = T>(id: string, throwIfNotFound?: true): Promise<U>;
