475d175948291680ef9b79c07bbd03fba62fe2bc
Patrick Hein <bagbag98@googlemail.com>
2020-07-31 08:55:25 +0200

    updates angular

--- angular/projects/tstdl-angular/src/public-api.ts
+++ angular/projects/tstdl-angular/src/public-api.ts
@@ -3,5 +3,7 @@
  */
 
 export * from './lib/pipes/localize.pipe';
+export * from './lib/services/app-stabilization.service';
 export * from './lib/services/localization.service';
 export * from './lib/tstdl-angular.module';
+
