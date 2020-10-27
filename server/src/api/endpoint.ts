1eb95b482f277a11325dca47890bd624dd9a2bec
Patrick Hein <bagbag98@googlemail.com>
2020-10-27 11:03:33 +0100

    adds transformed api endpoint

--- server/src/api/endpoint.ts
+++ server/src/api/endpoint.ts
@@ -1,9 +1,10 @@
-import { EndpointParametersValidator } from './validation';
+import type { EndpointParametersValidator } from './validation';
 
 export type ApiEndpoint<Parameters, Result, Context = unknown> = (parameters: Parameters, context: Context) => Result | Promise<Result>;
+export type ParametersUpdater<Parameters, UpdatedParameters> = (parameters: Parameters) => UpdatedParameters | Promise<UpdatedParameters>;
 
 export function createValidatedApiEndpoint<Parameters, ValidatedParameters, Result, Context>(validator: EndpointParametersValidator<Parameters, ValidatedParameters>, handler: ApiEndpoint<ValidatedParameters, Result, Context>): ApiEndpoint<Parameters, Result, Context> {
-  const endpoint: (parameters: Parameters, context: Context) => Promise<Result> = async (parameters: Parameters, context: Context) => {
+  async function validationApiEndpoint(parameters: Parameters, context: Context): Promise<Result> {
     const validationResult = await validator(parameters);
 
     if (!validationResult.valid) {
@@ -11,7 +12,16 @@ export function createValidatedApiEndpoint<Parameters, ValidatedParameters, Resu
     }
 
     return handler(validationResult.value, context);
-  };
+  }
 
-  return endpoint;
+  return validationApiEndpoint;
+}
+
+export function createTransformedApiEndpoint<Parameters, UpdatedParameters, Result, Context>(updater: ParametersUpdater<Parameters, UpdatedParameters>, handler: ApiEndpoint<UpdatedParameters, Result, Context>): ApiEndpoint<Parameters, Result, Context> {
+  async function transformApiEndpoint(parameters: Parameters, context: Context): Promise<Result> {
+    const updatedParameters = await updater(parameters);
+    return handler(updatedParameters, context);
+  }
+
+  return transformApiEndpoint;
 }
