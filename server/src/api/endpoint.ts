e1696c7bfebc369cc2e63ad12018a3bd18b308b7
Patrick Hein <bagbag98@googlemail.com>
2020-10-27 11:04:42 +0100

    fixes naming

--- server/src/api/endpoint.ts
+++ server/src/api/endpoint.ts
@@ -1,7 +1,7 @@
 import type { EndpointParametersValidator } from './validation';
 
 export type ApiEndpoint<Parameters, Result, Context = unknown> = (parameters: Parameters, context: Context) => Result | Promise<Result>;
-export type ParametersUpdater<Parameters, UpdatedParameters> = (parameters: Parameters) => UpdatedParameters | Promise<UpdatedParameters>;
+export type ParametersTransformer<Parameters, UpdatedParameters> = (parameters: Parameters) => UpdatedParameters | Promise<UpdatedParameters>;
 
 export function createValidatedApiEndpoint<Parameters, ValidatedParameters, Result, Context>(validator: EndpointParametersValidator<Parameters, ValidatedParameters>, handler: ApiEndpoint<ValidatedParameters, Result, Context>): ApiEndpoint<Parameters, Result, Context> {
   async function validationApiEndpoint(parameters: Parameters, context: Context): Promise<Result> {
@@ -17,10 +17,10 @@ export function createValidatedApiEndpoint<Parameters, ValidatedParameters, Resu
   return validationApiEndpoint;
 }
 
-export function createTransformedApiEndpoint<Parameters, UpdatedParameters, Result, Context>(updater: ParametersUpdater<Parameters, UpdatedParameters>, handler: ApiEndpoint<UpdatedParameters, Result, Context>): ApiEndpoint<Parameters, Result, Context> {
+export function createTransformedApiEndpoint<Parameters, TransformedParameters, Result, Context>(transformer: ParametersTransformer<Parameters, TransformedParameters>, handler: ApiEndpoint<TransformedParameters, Result, Context>): ApiEndpoint<Parameters, Result, Context> {
   async function transformApiEndpoint(parameters: Parameters, context: Context): Promise<Result> {
-    const updatedParameters = await updater(parameters);
-    return handler(updatedParameters, context);
+    const transformedParameters = await transformer(parameters);
+    return handler(transformedParameters, context);
   }
 
   return transformApiEndpoint;
