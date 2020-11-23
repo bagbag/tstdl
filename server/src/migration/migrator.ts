92a2a1f4dee77c6384d7e968d233ab80e540b3c3
Patrick Hein <bagbag98@googlemail.com>
2020-11-23 11:13:28 +0100

    improves migrator

--- server/src/migration/migrator.ts
+++ server/src/migration/migrator.ts
@@ -1,18 +1,25 @@
 import type { LockProvider } from '@tstdl/base/lock';
 import type { Logger } from '@tstdl/base/logger';
-import { compareByValueSelectionDescending, isDefined, isUndefined, round, Timer, toArray } from '@tstdl/base/utils';
+import { compareByValueSelectionDescending, isDefined, round, Timer, toArray } from '@tstdl/base/utils';
 import type { NewMigrationState } from './migration-state';
 import type { MigrationStateRepository } from './migration-state-repository';
 
-export type MigrationDefinition = {
+export type MigrationDefinition<T = void> = {
   name: string,
-  migrations: Migration[]
+  migrations: Migration<T>[]
 };
 
 export type Migration<T = void> = {
   from: 'init' | number | number[],
   to: number,
   migrator: () => Promise<T> | T
+};
+
+export type MigrationResult<T> = {
+  from: 'init' | number,
+  to: number,
+  time: number,
+  result: T
 };
 
 export class Migrator {
@@ -27,23 +34,20 @@ export class Migrator {
   }
 
   // eslint-disable-next-line max-statements, max-lines-per-function
-  async migrate({ name, migrations }: MigrationDefinition): Promise<void> {
+  async migrate<T>({ name, migrations }: MigrationDefinition<T>): Promise<MigrationResult<T>[]> {
     if (migrations.length == 0) {
       throw new Error('no migrations provided');
     }
 
     const lock = this.lockProvider.get(`migrator-${name}`);
 
-    let hasLatest = false;
-
-    await lock.using(30000, true, async () => {
+    const { result } = await lock.using(30000, true, async (): Promise<MigrationResult<T> | false> => {
       const currentState = await this.migrationStateRepository.tryLoadByFilter({ name });
       const currentRevision = currentState?.revision ?? 'init';
       const latestRevision = migrations.sort(compareByValueSelectionDescending((migration) => migration.to))[0]!.to;
 
       if (currentRevision == latestRevision) {
-        hasLatest = true;
-        return;
+        return false;
       }
 
       const suitableMigrations = migrations.filter((migration) => toArray(migration.from).includes(currentRevision));
@@ -52,31 +56,34 @@ export class Migrator {
         throw new Error(`no suitable migration path from current revision ${currentRevision} to latest revision ${latestRevision} found`);
       }
 
-      const largestMigration = suitableMigrations.sort(compareByValueSelectionDescending((migration) => migration.to))[0]!;
+      const migration = suitableMigrations.sort(compareByValueSelectionDescending((m) => m.to))[0]!;
 
-      this.logger.warn(`starting migration for "${name}" from revision ${currentRevision} to ${largestMigration.to}`);
+      this.logger.warn(`starting migration for "${name}" from revision ${currentRevision} to ${migration.to}`);
 
-      const time = await Timer.measureAsync(async () => largestMigration.migrator());
+      let migratorResult!: T;
+      const time = await Timer.measureAsync(async () => (migratorResult = await migration.migrator()));
 
       if (isDefined(currentState)) {
-        await this.migrationStateRepository.patchByFilter({ name }, { revision: largestMigration.to });
+        await this.migrationStateRepository.patchByFilter({ name }, { revision: migration.to });
       }
       else {
         const newState: NewMigrationState = {
           name,
-          revision: largestMigration.to
+          revision: migration.to
         };
 
         await this.migrationStateRepository.insert(newState);
       }
 
       this.logger.warn(`finished migration in ${round(time / 1000, 2)} seconds`);
+
+      return { from: currentRevision, to: migration.to, time, result: migratorResult };
     });
 
-    if (hasLatest) {
-      return;
+    if (result == false) {
+      return [];
     }
 
-    await this.migrate({ name, migrations });
+    return [result, ...(await this.migrate({ name, migrations }))];
   }
 }
