3b237607ed0f38b7c1fe468fdfb7c27f3f11d1ad
Patrick Hein <bagbag98@googlemail.com>
2020-11-23 13:11:45 +0100

    implements migration-control

--- server/src/migration/migrator.ts
+++ server/src/migration/migrator.ts
@@ -1,6 +1,6 @@
 import type { LockProvider } from '@tstdl/base/lock';
 import type { Logger } from '@tstdl/base/logger';
-import { compareByValueSelectionDescending, isDefined, round, Timer, toArray } from '@tstdl/base/utils';
+import { CancellationToken, compareByValueSelectionDescending, isDefined, round, Timer, toArray } from '@tstdl/base/utils';
 import type { NewMigrationState } from './migration-state';
 import type { MigrationStateRepository } from './migration-state-repository';
 
@@ -12,14 +12,19 @@ export type MigrationDefinition<T = void> = {
 export type Migration<T = void> = {
   from: 'init' | number | number[],
   to: number,
-  migrator: () => Promise<T> | T
+  migrator: (control: MigrationControl) => Promise<T> | T
+};
+
+export type MigrationControl = {
+  restart: () => void
 };
 
 export type MigrationResult<T> = {
   from: 'init' | number,
   to: number,
   time: number,
-  result: T
+  result: T,
+  restartRequested: boolean
 };
 
 export class Migrator {
@@ -41,49 +46,63 @@ export class Migrator {
 
     const lock = this.lockProvider.get(`migrator-${name}`);
 
-    const { result } = await lock.using(30000, true, async (): Promise<MigrationResult<T> | false> => {
-      const currentState = await this.migrationStateRepository.tryLoadByFilter({ name });
-      const currentRevision = currentState?.revision ?? 'init';
-      const latestRevision = migrations.sort(compareByValueSelectionDescending((migration) => migration.to))[0]!.to;
+    // eslint-disable-next-line max-statements, max-lines-per-function
+    const { result } = await lock.using(30000, true, async (): Promise<MigrationResult<T>[]> => {
+      const results: MigrationResult<T>[] = [];
 
-      if (currentRevision == latestRevision) {
-        return false;
-      }
-
-      const suitableMigrations = migrations.filter((migration) => toArray(migration.from).includes(currentRevision));
-
-      if (suitableMigrations.length == 0) {
-        throw new Error(`no suitable migration path from current revision ${currentRevision} to latest revision ${latestRevision} found`);
-      }
-
-      const migration = suitableMigrations.sort(compareByValueSelectionDescending((m) => m.to))[0]!;
-
-      this.logger.warn(`starting migration for "${name}" from revision ${currentRevision} to ${migration.to}`);
-
-      let migratorResult!: T;
-      const time = await Timer.measureAsync(async () => (migratorResult = await migration.migrator()));
-
-      if (isDefined(currentState)) {
-        await this.migrationStateRepository.patchByFilter({ name }, { revision: migration.to });
-      }
-      else {
-        const newState: NewMigrationState = {
-          name,
-          revision: migration.to
+      while (true) {
+        const restartToken = new CancellationToken();
+        const control: MigrationControl = {
+          restart: () => restartToken.set()
         };
 
-        await this.migrationStateRepository.insert(newState);
+        const currentState = await this.migrationStateRepository.tryLoadByFilter({ name });
+        const currentRevision = currentState?.revision ?? 'init';
+        const latestRevision = migrations.sort(compareByValueSelectionDescending((migration) => migration.to))[0]!.to;
+
+        if (currentRevision == latestRevision) {
+          break;
+        }
+
+        const suitableMigrations = migrations.filter((migration) => toArray(migration.from).includes(currentRevision));
+
+        if (suitableMigrations.length == 0) {
+          throw new Error(`no suitable migration path from current revision ${currentRevision} to latest revision ${latestRevision} found`);
+        }
+
+        const migration = suitableMigrations.sort(compareByValueSelectionDescending((m) => m.to))[0]!;
+
+        this.logger.warn(`starting migration for "${name}" from revision ${currentRevision} to ${migration.to}`);
+
+        let migratorResult!: T;
+        const time = await Timer.measureAsync(async () => (migratorResult = await migration.migrator(control)));
+
+        results.push({ from: currentRevision, to: migration.to, time, result: migratorResult, restartRequested: restartToken.isSet });
+
+        if (restartToken.isSet) {
+          this.logger.warn(`finished migration in ${round(time / 1000, 2)} seconds`);
+          this.logger.warn('migration-restart requested');
+          continue;
+        }
+
+        if (isDefined(currentState)) {
+          await this.migrationStateRepository.patchByFilter({ name }, { revision: migration.to });
+        }
+        else {
+          const newState: NewMigrationState = {
+            name,
+            revision: migration.to
+          };
+
+          await this.migrationStateRepository.insert(newState);
+        }
+
+        this.logger.warn(`finished migration in ${round(time / 1000, 2)} seconds`);
       }
 
-      this.logger.warn(`finished migration in ${round(time / 1000, 2)} seconds`);
-
-      return { from: currentRevision, to: migration.to, time, result: migratorResult };
+      return results;
     });
 
-    if (result == false) {
-      return [];
-    }
-
-    return [result, ...(await this.migrate({ name, migrations }))];
+    return result;
   }
 }
