2419c9f87bd236ec1e2c7e17c4e84197e50b6877
Patrick Hein <bagbag98@googlemail.com>
2021-07-15 11:45:17 +0200

    fix mongo instance provider

--- base/source/mongo.instance-provider.ts
+++ base/source/mongo.instance-provider.ts
@@ -57,7 +57,6 @@ let mongoKeyValueRepositoryConfig: MongoRepositoryConfig<MongoKeyValue> | undefi
 
 export function configureMongoInstanceProvider(
   options: {
-    connectionString?: string,
     defaultDatabase?: string,
     defaultConnection?: MongoConnection,
     mongoLogPrefix?: string,
