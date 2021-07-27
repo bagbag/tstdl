919af53d861cb7cb08cf829c6e78afc87f1fbedc
Patrick Hein <bagbag98@googlemail.com>
2021-07-27 20:02:54 +0200

    fix

--- angular/projects/tstdl-angular/src/lib/utils/route-data.ts
+++ angular/projects/tstdl-angular/src/lib/utils/route-data.ts
@@ -1,8 +1,8 @@
 import type { Resolve } from '@angular/router';
-import type { StringMap } from '@tstdl/base/esm/types';
+import type { StringMap, Type } from '@tstdl/base/esm/types';
 
 export type RouteDataDefinition<T extends StringMap> = {
-  [P in keyof T]: T[P] | Resolve<T[P]>
+  [P in keyof T]: T[P] | Type<Resolve<T[P]>>
 };
 
 export function routeData<T extends StringMap>(definition: RouteDataDefinition<T>): RouteDataDefinition<T> {
