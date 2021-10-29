64d5ed3c0833e55c68f2c07b9c1290777b775526
Patrick Hein <bagbag98@googlemail.com>
2021-10-29 18:57:41 +0200

    update

--- base/source/rxjs/timing.ts
+++ base/source/rxjs/timing.ts
@@ -1,16 +1,16 @@
 import { Observable } from 'rxjs';
 
-export const singleImmediate$ = singleCallback(setImmediate, clearImmediate);
-export const immediate$ = recursiveCallback(setImmediate, clearImmediate);
+export const singleIdle$ = singleCallback((callback) => requestIdleCallback(callback), (handle) => cancelIdleCallback(handle));
+export const idle$ = recursiveCallback((callback) => requestIdleCallback(callback), (handle) => cancelIdleCallback(handle));
+
+export const singleAnimationFrame$ = singleCallback((callback) => requestAnimationFrame(callback), (handle) => cancelAnimationFrame(handle));
+export const animationFrame$ = recursiveCallback((callback) => requestAnimationFrame(callback), (handle) => cancelAnimationFrame(handle));
+
+export const singleImmediate$ = singleCallback((callback) => setImmediate(callback), (handle) => clearImmediate(handle));
+export const immediate$ = recursiveCallback((callback) => setImmediate(callback), (handle) => clearImmediate(handle));
 
 export const singleNextTick$ = singleCallback((callback) => process.nextTick(callback));
 export const nextTick$ = recursiveCallback((callback) => process.nextTick(callback));
-
-export const singleIdle$ = singleCallback(requestIdleCallback, cancelIdleCallback);
-export const idle$ = recursiveCallback(requestIdleCallback, cancelIdleCallback);
-
-export const singleAnimationFrame$ = singleCallback(requestAnimationFrame, cancelAnimationFrame);
-export const animationFrame$ = recursiveCallback(requestAnimationFrame, cancelAnimationFrame);
 
 function singleCallback<T, H>(scheduler: (callback: (value: T) => void) => H, canceller?: (handle: H) => void): Observable<T> {
   return new Observable<T>((subscriber) => {
