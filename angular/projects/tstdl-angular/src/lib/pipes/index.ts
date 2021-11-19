d61ea4a753d29b1886cbae7df7e2f89fb674e773
Patrick Hein <bagbag98@googlemail.com>
2021-11-19 15:46:07 +0100

    fix optional-localize pipe export

--- angular/projects/tstdl-angular/src/lib/pipes/index.ts
+++ angular/projects/tstdl-angular/src/lib/pipes/index.ts
@@ -4,6 +4,7 @@ export * from './date-time.pipe';
 export * from './duration.pipe';
 export * from './localize.pipe';
 export * from './numeric-date-to-date-time.pipe';
+export * from './optional-localize.pipe';
 export * from './pad.pipe';
 export * from './safe-url.pipe';
 export * from './timestamp-to-date-time.pipe';
--- angular/projects/tstdl-angular/src/lib/tstdl-angular.module.ts
+++ angular/projects/tstdl-angular/src/lib/tstdl-angular.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { LazyListDirective, VisibleObserverDirective } from './directives';
 import { LetDirective } from './directives/let.directive';
-import { DateTimeLocalePipe, DateTimePipe, DateTimeToDatePipe, DurationPipe, LocalizePipe, NumericDateToDateTimePipe, PadPipe, SafeUrlPipe, TimestampToDateTimePipe } from './pipes';
+import { DateTimeLocalePipe, DateTimePipe, DateTimeToDatePipe, DurationPipe, LocalizePipe, NumericDateToDateTimePipe, OptionalLocalizePipe, PadPipe, SafeUrlPipe, TimestampToDateTimePipe } from './pipes';
 
 const declarations = [
   DateTimeLocalePipe,
@@ -11,6 +11,7 @@ const declarations = [
   LazyListDirective,
   LetDirective,
   LocalizePipe,
+  OptionalLocalizePipe,
   NumericDateToDateTimePipe,
   PadPipe,
   SafeUrlPipe,
