eef0b52bd867889bed62fd21e61371ae9336fbeb
Patrick Hein <bagbag98@googlemail.com>
2022-02-11 13:51:10 +0100

    update lazy list

--- angular/projects/tstdl-angular/src/lib/directives/lazy-list.directive.ts
+++ angular/projects/tstdl-angular/src/lib/directives/lazy-list.directive.ts
@@ -1,15 +1,15 @@
 import type { AfterViewInit } from '@angular/core';
 import { ChangeDetectorRef, Directive, ElementRef, Input } from '@angular/core';
-import { animationFrame$, observeIntersection, observeResize } from '@tstdl/base/rxjs';
+import { observeIntersection, observeResize } from '@tstdl/base/rxjs';
 import { isUndefined, timeout } from '@tstdl/base/utils';
 import type { Observable } from 'rxjs';
-import { BehaviorSubject, combineLatest, EMPTY, filter, fromEvent, map, merge, switchMap, take } from 'rxjs';
-import { LifecycleUtils } from '../utils';
+import { BehaviorSubject, combineLatest, EMPTY, filter, fromEvent, map, merge, switchMap, switchMapTo, take } from 'rxjs';
+import { LifecycleUtils } from '../utils/lifecycle';
 
 type MaybeWithNativeElement = HTMLElement & Partial<ElementRef<HTMLElement>>;
 
 @Directive({
-  selector: '[tstdlLazyList]',
+  selector: '[tslLazyList]',
   exportAs: 'lazyList'
 })
 export class LazyListDirective<T> extends LifecycleUtils<LazyListDirective<T>> implements AfterViewInit {
@@ -25,7 +25,7 @@ export class LazyListDirective<T> extends LifecycleUtils<LazyListDirective<T>> i
   /**
    * items to lazily append to {@link items}
    */
-  @Input('tstdlLazyList') source: T[];
+  @Input('tslLazyList') source: T[];
 
   /**
    * how many items to add at the first tick
@@ -40,7 +40,7 @@ export class LazyListDirective<T> extends LifecycleUtils<LazyListDirective<T>> i
   @Input('lazyBatchSize') batchSize: number;
 
   /**
-   * how far to preload items. Percentage of client height
+   * how far to preload items. Percentage of scroll elements client height
    * @default 20
    */
   @Input('lazyMargin') margin: number;
@@ -51,15 +51,15 @@ export class LazyListDirective<T> extends LifecycleUtils<LazyListDirective<T>> i
   @Input('lazyScrollElement') scrollElement: MaybeWithNativeElement | undefined;
 
   /**
-   * element to observe for intersection and load items when intersecting
+   * element to observe for intersection with the scroll element to trigger at tick
    */
   @Input('lazyObserveElement') observeElement: MaybeWithNativeElement | undefined;
 
   get thresholdReached(): boolean {
     const { scrollHeight, scrollTop, clientHeight } = this.scrollElementSubject.value;
-
     const threshold = Math.max(1, clientHeight * (this.margin / 100));
-    return (scrollHeight - scrollTop - clientHeight) <= threshold;
+
+    return (clientHeight > 0) && ((scrollHeight - scrollTop - clientHeight) <= threshold);
   }
 
   get hasAll(): boolean {
@@ -99,15 +99,20 @@ export class LazyListDirective<T> extends LifecycleUtils<LazyListDirective<T>> i
     );
 
     const scroll$ = this.scrollElement$.pipe(
-      switchMap((element) => fromEvent(element, 'scroll'))
+      switchMap((element) => fromEvent(element, 'scroll', { passive: true } as AddEventListenerOptions))
     );
 
-    const intersect$ = combineLatest([this.scrollElement$, this.observeElement$]).pipe(
-      switchMap(([scrollElement, observeElement]) => (isUndefined(observeElement) ? EMPTY : observeIntersection(observeElement, { root: scrollElement, rootMargin: `${this.margin}%` }))),
+    const intersect$ = combineLatest([this.scrollElement$, this.observeElement$, this.observe('margin')]).pipe(
+      switchMap(([scrollElement, observeElement, margin]) => (isUndefined(observeElement) ? EMPTY : observeIntersection(observeElement, { root: scrollElement, rootMargin: `${margin}%` }))),
       filter((entries) => entries[0]!.isIntersecting)
     );
 
-    merge(this.observe('source'), animationFrame$.pipe(take(3)), resize$, scroll$, intersect$).subscribe(() => void this.check());
+    this.viewChecked$
+      .pipe(
+        take(1),
+        switchMapTo(merge(this.observe('source'), resize$, scroll$, intersect$))
+      )
+      .subscribe(() => void this.check());
   }
 
   async check(): Promise<void> {
@@ -115,17 +120,14 @@ export class LazyListDirective<T> extends LifecycleUtils<LazyListDirective<T>> i
       return;
     }
 
+    this.checking = true;
+
     while (!this.hasAll && this.thresholdReached) {
-      this.addItems();
-      await timeout(0);
-      void this.check();
+      this.items = this.source.slice(0, Math.max(this.initialSize, this.items.length + this.batchSize));
+      this.changeDetector.markForCheck();
+      await timeout();
     }
 
     this.checking = false;
-  }
-
-  private addItems(): void {
-    this.items = this.source.slice(0, Math.max(this.initialSize, this.items.length + this.batchSize));
-    this.changeDetector.markForCheck();
   }
 }
