daeb022599cee8117c629e4687825235cc258b62
Patrick Hein <bagbag98@googlemail.com>
2022-02-17 10:38:45 +0100

    fix export

--- angular/projects/tstdl-angular/src/lib/directives/index.ts
+++ angular/projects/tstdl-angular/src/lib/directives/index.ts
@@ -1,4 +1,5 @@
 export * from './lazy-list.directive';
 export * from './lazy.directive';
 export * from './let.directive';
+export * from './repeat.directive';
 export * from './visibility-observer.directive';
