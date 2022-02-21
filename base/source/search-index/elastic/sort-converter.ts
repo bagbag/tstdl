67c7837c31193445cb61f1e8aa25101de987789d
Patrick Hein <bagbag98@googlemail.com>
2022-02-21 12:49:49 +0100

    fix elasticsearch sort

--- base/source/search-index/elastic/sort-converter.ts
+++ base/source/search-index/elastic/sort-converter.ts
@@ -7,11 +7,15 @@ const renameMap = new Map([
 ]);
 
 // eslint-disable-next-line max-lines-per-function, max-statements, complexity
-export function convertSort<T extends Entity>(sort: Sort<T>, keywordRewrites: Set<string>): SortCombinations {
-  const name = renameMap.get(sort.field as string) ?? sort.field as string;
+export function convertSort<T extends Entity>(sort: Sort<T>, keywordRewrites: Set<string>): SortCombinations<T> {
+  const name = renameMap.get(sort.field as string) ?? sort.field;
   const rewrite = keywordRewrites.has(name);
   const field = rewrite ? `${name}.keyword` : name;
   const order: SortOrder = sort.order ?? 'asc';
 
-  return `${field}:${order}`;
+  if (((field != '_score') && (order == 'asc')) || ((field == '_score') && (order == 'desc'))) {
+    return field as keyof T;
+  }
+
+  return { [field]: order } as SortCombinations<T>;
 }
