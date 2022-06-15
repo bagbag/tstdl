50cd533e7510114604e04edcf0424050365031c2
Patrick Hein <bagbag98@googlemail.com>
2022-06-15 13:07:31 +0200

    add pdf render options

--- base/source/pdf/pdf.service.ts
+++ base/source/pdf/pdf.service.ts
@@ -7,6 +7,22 @@ import { Pool } from '#/pool';
 import { TemplateService } from '#/templates';
 import * as puppeteer from 'puppeteer';
 
+export type PdfRenderOptions = {
+  omitDefaultBackground?: boolean,
+  renderBackground?: boolean,
+  landscape?: boolean,
+  format?: 'letter' | 'legal' | 'tabloid' | 'ledger' | 'a0' | 'a1' | 'a2' | 'a3' | 'a4' | 'a5' | 'a6',
+  width?: string | number,
+  height?: string | number,
+  scale?: number,
+  margin?: {
+    top?: number | string,
+    bottom?: number | string,
+    right?: number | string,
+    left?: number | string
+  }
+};
+
 @singleton()
 export class PdfService implements AsyncDisposable, AfterResolve {
   private readonly templateService: TemplateService;
@@ -33,26 +49,36 @@ export class PdfService implements AsyncDisposable, AfterResolve {
     return this.pool.dispose();
   }
 
-  async renderHtml(html: string): Promise<Uint8Array> {
-    return this.render(async (page) => page.setContent(html, { waitUntil: 'networkidle2' }));
+  async renderHtml(html: string, options?: PdfRenderOptions): Promise<Uint8Array> {
+    return this.render(async (page) => page.setContent(html, { waitUntil: 'networkidle2' }), options);
   }
 
-  async renderUrl(url: string): Promise<Uint8Array> {
-    return this.render(async (page) => page.goto(url, { waitUntil: 'networkidle2' }));
+  async renderUrl(url: string, options?: PdfRenderOptions): Promise<Uint8Array> {
+    return this.render(async (page) => page.goto(url, { waitUntil: 'networkidle2' }), options);
   }
 
-  async renderTemplate(key: string, templateContext: object): Promise<Uint8Array> {
+  async renderTemplate(key: string, templateContext?: object, options?: PdfRenderOptions): Promise<Uint8Array> {
     const html = await this.templateService.render(key, templateContext);
-    return this.renderHtml(html);
+    return this.renderHtml(html, options);
   }
 
-  private async render(handler: (page: puppeteer.Page) => unknown): Promise<Uint8Array> {
+  private async render(handler: (page: puppeteer.Page) => unknown, options?: PdfRenderOptions): Promise<Uint8Array> {
     return this.pool.use(async (browser) => {
       const page = await browser.newPage();
 
       try {
         await handler(page);
-        const result = await page.pdf({ format: 'a4' });
+        const result = await page.pdf({
+          format: options?.format ?? 'a4',
+          scale: options?.scale,
+          landscape: options?.landscape,
+          width: options?.width,
+          height: options?.height,
+          omitBackground: options?.omitDefaultBackground,
+          printBackground: options?.renderBackground,
+          margin: options?.margin
+        });
+
         return result;
       }
       finally {
