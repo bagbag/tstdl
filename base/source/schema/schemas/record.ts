61a3578a6da62108c0e2fad8d27e57545115bdfe
Patrick Hein <bagbag98@googlemail.com>
2022-08-08 22:24:12 +0200

    update

--- base/source/schema/schemas/record.ts
+++ base/source/schema/schemas/record.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-import type { Record, TypedOmit } from '#/types';
-import type { MaybeDeferredValueTypes, ObjectSchema } from '../types';
+import type { OneOrMany, Record, TypedOmit } from '#/types';
+import type { ObjectSchema, ValueType } from '../types';
 import { objectSchema } from '../types';
 
-export type RecordOptions<T extends Record = Record> = TypedOmit<ObjectSchema<T>, 'properties' | 'allowUnknownProperties'>;
+export type RecordOptions<T extends Record = Record> = TypedOmit<ObjectSchema<T>, 'properties' | 'allowUnknownProperties' | 'mask'>;
 
-export function record<T extends MaybeDeferredValueTypes>(schemas: T, options?: RecordOptions): ObjectSchema<Record<any, T extends MaybeDeferredValueTypes<infer U> ? U : never>> {
+export function record<T, O>(valueType: OneOrMany<ValueType<T, O>>, options?: RecordOptions): ObjectSchema<Record<any, O>> {
   return objectSchema({
     properties: {},
-    allowUnknownProperties: schemas,
+    allowUnknownProperties: valueType,
     ...options
   }) as ObjectSchema;
 }
