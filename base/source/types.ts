5df750014dde259b0fe86ff05cc0077dbb66908a
Patrick Hein <bagbag98@googlemail.com>
2022-08-19 15:07:56 +0200

    update dependencies

--- base/source/types.ts
+++ base/source/types.ts
@@ -149,3 +149,20 @@ export type TypedArray =
   | BigUint64Array;
 
 export type BinaryData = ArrayBuffer | ArrayBufferView;
+
+type JoinPaths<K, P> = K extends string | number ?
+  P extends string | number ?
+  `${K}${'' extends P ? '' : '.'}${P}`
+  : never : never;
+
+type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]];
+
+export type Paths<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
+  { [K in keyof T]-?: K extends string | number ?
+    `${K}` | (Paths<T[K], Prev[D]> extends infer R ? JoinPaths<K, R> : never)
+    : never
+  }[keyof T] : '';
+
+export type LeavePaths<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
+  { [K in keyof T]-?: JoinPaths<K, LeavePaths<T[K], Prev[D]>> }[keyof T] : '';
