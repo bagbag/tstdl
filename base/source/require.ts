ee5fa7132c859289abc41959df08d0ba330ff5ee
Patrick Hein <bagbag98@googlemail.com>
2022-10-28 16:45:58 +0200

    update

--- base/source/require.ts
+++ base/source/require.ts
@@ -1,4 +1,5 @@
+const _require = globalThis.require as typeof dynamicRequire;
+
 export function dynamicRequire<T = any>(id: string): T {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-  return require(id) as T;
+  return _require(id);
 }
