1afdb01a82ead4d18209505e753a9816384ccdeb
Patrick Hein <patrick-hein@outlook.com>
2023-05-07 19:03:51 +0200

    update

--- angular/projects/tstdl/angular/lib/source/types.ts
+++ angular/projects/tstdl/angular/lib/source/types.ts
@@ -1,4 +1,14 @@
+import type { Signal } from '@angular/core';
+import type { } from '@tstdl/base/signals/api';
 import type { Observable } from 'rxjs';
-import type { Signal } from './signals';
 
 export type ReactiveValue<T> = T | Signal<T> | Observable<T>;
+
+declare const SIGNAL: keyof Signal<any>;
+
+declare module '@tstdl/base/signals/api' {
+  interface Signal<T> {
+    (): T;
+    [SIGNAL]: unknown;
+  }
+}
--- angular/projects/tstdl/angular/lib/source/utils/route-data.ts
+++ angular/projects/tstdl/angular/lib/source/utils/route-data.ts
@@ -1,10 +1,10 @@
-import type { Resolve } from '@angular/router';
-import type { StringMap, Type } from '@tstdl/base/types';
+import type { ResolveFn } from '@angular/router';
+import type { Record } from '@tstdl/base/types';
 
-export type RouteDataDefinition<T extends StringMap> = {
-  [P in keyof T]: T[P] | Type<Resolve<T[P]>>
+export type RouteDataDefinition<T extends Record> = {
+  [P in keyof T]: T[P] | ResolveFn<T>;
 };
 
-export function routeData<T extends StringMap>(definition: RouteDataDefinition<T>): RouteDataDefinition<T> {
+export function routeData<T extends Record>(definition: RouteDataDefinition<T>): RouteDataDefinition<T> {
   return definition;
 }
