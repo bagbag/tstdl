3e053dd435471ce95620d31573b807cd27f5907e
Patrick Hein <patrick-hein@outlook.com>
2023-05-29 12:17:18 +0200

    update

--- angular/projects/tstdl/angular/lib/source/services/index.ts
+++ angular/projects/tstdl/angular/lib/source/services/index.ts
@@ -1,5 +1,6 @@
 export * from './app-stabilization.service';
 export * from './application-tick.service';
+export * from './error-handler-message.service';
 export * from './error-handler.service';
 export * from './notification.service';
 export * from './tstdl-bridge.service';
