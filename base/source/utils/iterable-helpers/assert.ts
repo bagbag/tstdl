eb9cb25fd028443f8729ff9c00b1a4009f542d19
Patrick Hein <patrick-hein@outlook.com>
2023-06-11 16:41:30 +0200

    fix

--- base/source/utils/iterable-helpers/assert.ts
+++ base/source/utils/iterable-helpers/assert.ts
@@ -1,11 +1,11 @@
 import { assert as assertHelper } from '../type-guards.js';
 import type { Predicate, TypePredicate } from './types.js';
 
-export function* assert<T, TPredicate extends T = T>(iterable: Iterable<T>, predicate: Predicate<T> | TypePredicate<T, TPredicate>): IterableIterator<TPredicate> {
+export function* assert<T, TPredicate extends T = T>(iterable: Iterable<T>, predicate: TypePredicate<T, TPredicate> | Predicate<T>): IterableIterator<TPredicate> {
   let index = 0;
 
   for (const item of iterable) {
     assertHelper(predicate(item, index++));
-    yield item;
+    yield item as TPredicate;
   }
 }
