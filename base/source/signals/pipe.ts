b31f31cc5ee5f09ba71802ab66d0a9f080005d95
Patrick Hein <patrick-hein@outlook.com>
2023-06-30 14:08:17 +0200

    update

--- base/source/signals/pipe.ts
+++ base/source/signals/pipe.ts
@@ -1,52 +1,41 @@
 import { distinctUntilChanged, startWith, type Observable, type OperatorFunction } from 'rxjs';
 
-import { isFunction } from '#/utils/type-guards.js';
 import type { Signal } from './api.js';
 import { toObservable, toSignal } from './api.js';
 
-export type SignalPipeOptions = {
-  /**
-   * As we need an synchronous value for the resulting signal and effect scheduling is async, use the signals value as the initial value for the pipe.
-   * This would normally result in double emission inside the pipe, but is mitigated by using {@link SignalPipeOptions.distinctUntilChanged}
-   * @default true
-   */
-  startWithSignalValue?: boolean,
+/**
+ * As we need an synchronous value for the resulting signal and effect scheduling is async, it uses the source signals value as the initial value for the pipe.
+ * This would normally result in double emission inside the pipe, but is mitigated by using {@link SignalPipeOptions.distinctUntilChanged}. Because of this, mutating source signals are not supported.
+ * If you have an mutating signal or require more control for whatever reason, use {@link rawPipe} instead.
+ */
+export function pipe<T, A>(source: Signal<T>, op1: OperatorFunction<T, A>): Signal<A>;
+export function pipe<T, A, B>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>): Signal<B>;
+export function pipe<T, A, B, C>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>): Signal<C>;
+export function pipe<T, A, B, C, D>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>): Signal<D>;
+export function pipe<T, A, B, C, D, E>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>): Signal<E>;
+export function pipe<T, A, B, C, D, E, F>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>): Signal<F>;
+export function pipe<T, A, B, C, D, E, F, G>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>): Signal<G>;
+export function pipe<T, A, B, C, D, E, F, G, H>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>, op8: OperatorFunction<G, H>): Signal<H>;
+export function pipe<T, A, B, C, D, E, F, G, H, I>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>, op8: OperatorFunction<G, H>, op9: OperatorFunction<H, I>): Signal<I>;
+export function pipe(source: Signal<unknown>, ...operators: OperatorFunction<unknown, unknown>[]): Signal<unknown>;
+export function pipe(source: Signal<unknown>, ...operators: OperatorFunction<unknown, unknown>[]): Signal<unknown> {
+  return rawPipe(source, startWith(source()), distinctUntilChanged(), ...operators);
+}
 
-  /**
-   * Whether to distinctUntilChanged source values. This must be disabled if signal mutation is used.
-   * @default startWithSignalValue
-   */
-  distinctUntilChanged?: boolean
-};
-
-export function pipe<T, A>(signal: Signal<T>, op1: OperatorFunction<T, A>, options?: SignalPipeOptions): Signal<A>;
-export function pipe<T, A, B>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, options?: SignalPipeOptions): Signal<B>;
-export function pipe<T, A, B, C>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, options?: SignalPipeOptions): Signal<C>;
-export function pipe<T, A, B, C, D>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, options?: SignalPipeOptions): Signal<D>;
-export function pipe<T, A, B, C, D, E>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, options?: SignalPipeOptions): Signal<E>;
-export function pipe<T, A, B, C, D, E, F>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, options?: SignalPipeOptions): Signal<F>;
-export function pipe<T, A, B, C, D, E, F, G>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>, options?: SignalPipeOptions): Signal<G>;
-export function pipe<T, A, B, C, D, E, F, G, H>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>, op8: OperatorFunction<G, H>, options?: SignalPipeOptions): Signal<H>;
-export function pipe<T, A, B, C, D, E, F, G, H, I>(signal: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>, op8: OperatorFunction<G, H>, op9: OperatorFunction<H, I>, options?: SignalPipeOptions): Signal<I>;
-export function pipe<T, U>(signal: Signal<T>, ...operators: (OperatorFunction<any, any> | SignalPipeOptions | undefined)[]): Signal<U> {
-  const last = operators.at(-1);
-  const lastIsOperator = isFunction(last);
-
-  const ops = (lastIsOperator ? operators : operators.slice(0, -1)) as OperatorFunction<any, any>[];
-  const { startWithSignalValue = true, distinctUntilChanged: distinct } = (lastIsOperator ? undefined : last) ?? {};
-
-  const optionOperators: OperatorFunction<any, any>[] = [];
-
-  if (startWithSignalValue) {
-    optionOperators.push(startWith(signal()));
-  }
-
-  if (distinct ?? startWithSignalValue) {
-    optionOperators.push(distinctUntilChanged());
-  }
-
-  const observable = toObservable(signal);
-  const piped = (observable.pipe as (...operators: OperatorFunction<any, any>[]) => any)(...optionOperators, ...ops) as Observable<U>;
-
+/**
+ * As we need an synchronous value for the resulting signal and effect scheduling is async, you have to provide an immediate value on subscription, for example via the {@link startWith} operator, otherwise an error is thrown.
+ */
+export function rawPipe<T, A>(source: Signal<T>, op1: OperatorFunction<T, A>): Signal<A>;
+export function rawPipe<T, A, B>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>): Signal<B>;
+export function rawPipe<T, A, B, C>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>): Signal<C>;
+export function rawPipe<T, A, B, C, D>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>): Signal<D>;
+export function rawPipe<T, A, B, C, D, E>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>): Signal<E>;
+export function rawPipe<T, A, B, C, D, E, F>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>): Signal<F>;
+export function rawPipe<T, A, B, C, D, E, F, G>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>): Signal<G>;
+export function rawPipe<T, A, B, C, D, E, F, G, H>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>, op8: OperatorFunction<G, H>): Signal<H>;
+export function rawPipe<T, A, B, C, D, E, F, G, H, I>(source: Signal<T>, op1: OperatorFunction<T, A>, op2: OperatorFunction<A, B>, op3: OperatorFunction<B, C>, op4: OperatorFunction<C, D>, op5: OperatorFunction<D, E>, op6: OperatorFunction<E, F>, op7: OperatorFunction<F, G>, op8: OperatorFunction<G, H>, op9: OperatorFunction<H, I>): Signal<I>;
+export function rawPipe(source: Signal<unknown>, ...operators: OperatorFunction<unknown, unknown>[]): Signal<unknown>;
+export function rawPipe(source: Signal<unknown>, ...operators: OperatorFunction<unknown, unknown>[]): Signal<unknown> {
+  const piped = (toObservable(source).pipe as (...operators: OperatorFunction<unknown, unknown>[]) => any)(...operators) as Observable<unknown>;
   return toSignal(piped, { requireSync: true });
 }
