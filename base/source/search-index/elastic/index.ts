ea4b2f144aa9b05a338563c3186a2496fde191a5
Patrick Hein <patrick-hein@outlook.com>
2023-09-13 20:04:55 +0200

    export elastic query builder and converter

--- base/source/search-index/elastic/index.ts
+++ base/source/search-index/elastic/index.ts
@@ -2,5 +2,7 @@ export * from './config.js';
 export * from './keyword-rewriter.js';
 export * from './model/index.js';
 export * from './module.js';
+export * from './query-builder/index.js';
+export * from './query-converter.js';
 export * from './search-index.js';
 export * from './types.js';
