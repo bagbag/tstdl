bca3567f0ad25c6d1655ef0207681600d4ba27c3
Patrick Hein <patrick-hein@outlook.com>
2023-10-15 00:56:57 +0200

    implement toBytesStream

--- base/source/utils/stream/index.ts
+++ base/source/utils/stream/index.ts
@@ -4,3 +4,4 @@ export * from './readable-stream-from-promise.js';
 export * from './size-limited-stream.js';
 export * from './stream-helper-types.js';
 export * from './stream-reader.js';
+export * from './to-bytes-stream.js';
---
+++ base/source/utils/stream/to-bytes-stream.ts
@@ -0,0 +1,98 @@
+import { isNotNull, isNull } from '../type-guards.js';
+
+export type ToBytesStreamOptions = {
+  ignoreCancel?: boolean
+};
+
+export function toBytesStream(stream: ReadableStream<ArrayBufferView>, options?: ToBytesStreamOptions): ReadableStream<Uint8Array> {
+  try { // try to use byob mode from source
+    let byobReader: ReadableStreamBYOBReader;
+
+    return new ReadableStream({
+      type: 'bytes',
+      autoAllocateChunkSize: 10240,
+      start() {
+        byobReader = stream.getReader({ mode: 'byob' });
+      },
+      async pull(controller) {
+        const readResult = await byobReader.read(controller.byobRequest!.view!);
+
+        if (readResult.done) {
+          controller.close();
+          controller.byobRequest!.respond(0);
+          return;
+        }
+
+        controller.byobRequest!.respond(readResult.value.byteLength);
+      },
+      async cancel(reason) {
+        if (options?.ignoreCancel == true) {
+          byobReader.releaseLock();
+        }
+        else {
+          await byobReader.cancel(reason);
+        }
+      }
+    });
+  }
+  catch { /* ignore */ }
+
+  let reader: ReadableStreamDefaultReader<ArrayBufferView>;
+  let buffer: ArrayBufferView | null = null;
+
+  return new ReadableStream({
+    type: 'bytes',
+    start() {
+      reader = stream.getReader();
+    },
+    async pull(controller) {
+      const isByobRequest = isNotNull(controller.byobRequest);
+      const bufferIsEmpty = isNull(buffer) || (buffer.byteLength == 0);
+
+      if (!isByobRequest && !bufferIsEmpty) {
+        // we stil have data left in buffer from previous pull which was byob
+        controller.enqueue(buffer!);
+        buffer = null;
+        return;
+      }
+
+      if (bufferIsEmpty) {
+        const readResult = await reader.read();
+
+        if (readResult.done) {
+          controller.close();
+          controller.byobRequest?.respond(0);
+          return;
+        }
+
+        buffer = readResult.value; // eslint-disable-line require-atomic-updates
+      }
+
+      if (!isByobRequest) {
+        controller.enqueue(buffer!);
+        buffer = null;
+        return;
+      }
+
+      const targetView = controller.byobRequest.view!;
+      const targetArray = new Uint8Array(targetView.buffer, targetView.byteOffset, targetView.byteLength);
+
+      const setLength = Math.min(buffer!.byteLength, targetArray.byteLength);
+      const sourceArray = new Uint8Array(buffer!.buffer, buffer!.byteOffset, setLength);
+
+      targetArray.set(sourceArray);
+
+      buffer = new Uint8Array(buffer!.buffer, buffer!.byteOffset + setLength);
+
+      controller.byobRequest.respond(setLength);
+    },
+    async cancel(reason) {
+      if (options?.ignoreCancel == true) {
+        reader.releaseLock();
+      }
+      else {
+        await reader.cancel(reason);
+      }
+    }
+  });
+}
