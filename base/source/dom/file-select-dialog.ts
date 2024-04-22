a7df131b26650e3ec020f703fe0c2b1234b514a8
Patrick Hein <patrick-hein@outlook.com>
2024-04-22 13:21:26 +0200

    implement open file select dialog

---
+++ base/source/dom/file-select-dialog.ts
@@ -0,0 +1,32 @@
+import type { OneOrMany } from '#/types.js';
+import { isDefined, isString } from '#/utils/type-guards.js';
+
+export type FileSelectDialogOptions = {
+  accept?: OneOrMany<string>,
+  multiple?: boolean,
+  capture?: string
+};
+
+export async function openFileSelectDialog({ accept, multiple, capture }: FileSelectDialogOptions = {}): Promise<File[] | null> {
+  const fileInput = document.createElement('input');
+  fileInput.type = 'file';
+
+  if (isDefined(accept)) {
+    fileInput.accept = isString(accept) ? accept : accept.join(',');
+  }
+
+  if (isDefined(multiple)) {
+    fileInput.multiple = multiple;
+  }
+
+  if (isDefined(capture)) {
+    fileInput.capture = capture;
+  }
+
+  fileInput.click();
+
+  return new Promise<File[] | null>((resolve) => {
+    fileInput.addEventListener('change', () => resolve(Array.from(fileInput.files ?? [])));
+    fileInput.addEventListener('cancel', () => resolve(null));
+  });
+}
