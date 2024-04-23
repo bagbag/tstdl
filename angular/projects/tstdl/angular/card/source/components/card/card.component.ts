9e5739d70df6f11b9e5654fef487b965f9f9079b
Patrick Hein <patrick-hein@outlook.com>
2024-04-23 10:55:50 +0200

    add left action slot to card

--- angular/projects/tstdl/angular/card/source/components/card/card.component.ts
+++ angular/projects/tstdl/angular/card/source/components/card/card.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren, Input, QueryList, type AfterViewInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, contentChildren, input } from '@angular/core';
 import { TstdlColoredProgressbarComponent, TstdlIndeterminateProgressBarComponent, fadeInOutAnimation } from '@tstdl/angular';
 import type { Record } from '@tstdl/base/types';
-import { merge } from 'rxjs';
 import { CardActionDirective, CardBodyDirective, CardFooterDirective, CardHeaderDirective, CardPostHeaderDirective, CardPreHeaderDirective, CardSubHeaderDirective } from '../../directives';
 
 @Component({
@@ -17,67 +16,32 @@ import { CardActionDirective, CardBodyDirective, CardFooterDirective, CardHeader
   ],
   host: {
     class: 'tsl-tw relative flex flex-col gap-4 rounded-xl shadow-lg bg-white dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100 outline outline-1 outline-neutral-600/10 dark:outline-neutral-100/10 overflow-hidden',
-    '[class.py-5]': 'padding',
-    '[class.px-6]': 'padding'
+    '[class.py-5]': 'padding()',
+    '[class.px-6]': 'padding()'
   }
 })
-export class CardComponent implements AfterViewInit {
-  private readonly changeDetector: ChangeDetectorRef;
+export class CardComponent {
+  readonly preHeaders = contentChildren(CardPreHeaderDirective);
+  readonly headers = contentChildren(CardHeaderDirective);
+  readonly subHeaders = contentChildren(CardSubHeaderDirective);
+  readonly postHeaders = contentChildren(CardPostHeaderDirective);
+  readonly bodies = contentChildren(CardBodyDirective);
+  readonly footers = contentChildren(CardFooterDirective);
+  readonly actions = contentChildren(CardActionDirective);
 
-  @ContentChildren(CardPreHeaderDirective, { read: CardPreHeaderDirective }) preHeaders: QueryList<CardPreHeaderDirective>;
-  @ContentChildren(CardHeaderDirective, { read: CardHeaderDirective }) headers: QueryList<CardHeaderDirective>;
-  @ContentChildren(CardSubHeaderDirective, { read: CardSubHeaderDirective }) subHeaders: QueryList<CardSubHeaderDirective>;
-  @ContentChildren(CardPostHeaderDirective, { read: CardPostHeaderDirective }) postHeaders: QueryList<CardPostHeaderDirective>;
-  @ContentChildren(CardBodyDirective, { read: CardBodyDirective }) bodies: QueryList<CardBodyDirective>;
-  @ContentChildren(CardFooterDirective, { read: CardFooterDirective }) footers: QueryList<CardFooterDirective>;
-  @ContentChildren(CardActionDirective, { read: CardActionDirective }) actions: QueryList<CardActionDirective>;
+  readonly progress = input<number>();
+  readonly headerSeparator = input<boolean>(true);
+  readonly footerSeparator = input<boolean>(true);
+  readonly padding = input<boolean>(true);
+  readonly loading = input<boolean>(false);
+  readonly footerBackground = input<boolean>(true);
+  readonly headerContainerClass = input<string | string[] | Set<string> | Record<string> | null | undefined>();
+  readonly headerOuterContainerClass = input<string | string[] | Set<string> | Record<string> | null | undefined>();
 
-  @Input() progress: number | undefined;
-  @Input() headerSeparator: boolean;
-  @Input() footerSeparator: boolean;
-  @Input() padding: boolean;
-  @Input() loading: boolean;
-  @Input() footerBackground: boolean;
-  @Input() headerContainerClass: string | string[] | Set<string> | Record<string> | null | undefined;
-  @Input() headerOuterContainerClass: string | string[] | Set<string> | Record<string> | null | undefined;
-
-  get hasHeader(): boolean {
-    return (this.headers.length + this.subHeaders.length + this.preHeaders.length + this.postHeaders.length) > 0;
-  }
-
-  get hasBody(): boolean {
-    return this.bodies.length > 0;
-  }
-
-  get hasFooter(): boolean {
-    return (this.footers.length + this.actions.length) > 0;
-  }
-
-  get hasAction(): boolean {
-    return this.actions.length > 0;
-  }
-
-  get showHeaderSeparator(): boolean {
-    return this.headerSeparator && this.hasHeader && this.hasBody;
-  }
-
-  get showFooterSeparator(): boolean {
-    return (this.progress == undefined) && this.footerSeparator && (this.hasHeader || this.hasBody);
-  }
-
-  constructor(changeDetector: ChangeDetectorRef) {
-    this.changeDetector = changeDetector;
-
-    this.progress = undefined;
-    this.headerSeparator = true;
-    this.footerSeparator = true;
-    this.footerBackground = true;
-    this.padding = true;
-    this.loading = false;
-  }
-
-  ngAfterViewInit(): void {
-    merge(this.preHeaders.changes, this.headers.changes, this.subHeaders.changes, this.postHeaders.changes, this.bodies.changes, this.footers.changes, this.actions.changes)
-      .subscribe(() => this.changeDetector.markForCheck());
-  }
+  readonly hasHeader = computed(() => (this.headers().length + this.subHeaders().length + this.preHeaders().length + this.postHeaders().length) > 0);
+  readonly hasBody = computed(() => this.bodies().length > 0);
+  readonly hasFooter = computed(() => (this.footers().length + this.actions().length) > 0);
+  readonly hasAction = computed(() => this.actions().length > 0);
+  readonly showHeaderSeparator = computed(() => this.headerSeparator() && this.hasHeader() && this.hasBody());
+  readonly showFooterSeparator = computed(() => (this.progress() == undefined) && this.footerSeparator() && (this.hasHeader() || this.hasBody()));
 }
