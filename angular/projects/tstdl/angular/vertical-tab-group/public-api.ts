134b23576207ee9060059fbba6acd2988fa40262
Patrick Hein <patrick-hein@outlook.com>
2024-06-26 12:04:28 +0200

    update

--- angular/projects/tstdl/angular/vertical-tab-group/public-api.ts
+++ angular/projects/tstdl/angular/vertical-tab-group/public-api.ts
@@ -1,2 +1,3 @@
 export * from './source/vertical-tab-group.component';
+export * from './source/vertical-tab/vertical-tab-label.directive';
 export * from './source/vertical-tab/vertical-tab.component';
