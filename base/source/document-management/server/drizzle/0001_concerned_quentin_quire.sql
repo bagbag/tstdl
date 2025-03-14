fc20688ec0d129c63008275f436efe8909a31434
Patrick Hein <patrick-hein@outlook.com>
2025-03-14 10:37:20 +0100

    update

--- base/source/document-management/server/drizzle/0001_concerned_quentin_quire.sql
+++ base/source/document-management/server/drizzle/0001_concerned_quentin_quire.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "document_management"."document" ADD COLUMN "validated" boolean NOT NULL;
+ALTER TABLE "document_management"."document" ADD COLUMN "validated" boolean NOT NULL DEFAULT true;
+ALTER TABLE "document_management"."document" ALTER COLUMN "validated" DROP DEFAULT;
